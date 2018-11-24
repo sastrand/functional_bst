ac0fc0b8daddc3651afa31b600acd6f69e429e76
sascha <sastrand@pdx.edu>
2018-11-24 11:12:32 -0800

    removed largestChild, expanded comments

--- bst.hs
+++ bst.hs
@@ -2,7 +2,7 @@ module BST where
 
 data BST a = EmptyLeaf | Node (BST a) a (BST a) deriving Show
 
--- insert (create, update)
+-- insert value (create, update)
 bstInsert :: (Ord a) => BST a -> a -> BST a
 bstInsert EmptyLeaf a = Node EmptyLeaf a EmptyLeaf
 bstInsert (Node l a r) b 
@@ -10,27 +10,19 @@ bstInsert (Node l a r) b
     | b > a  = Node l a (bstInsert r b)
     | b < a  = Node (bstInsert l b) a r
 
--- search (read)
+-- search for value (read)
 bstSearch :: (Ord a) => BST a -> a -> Bool
 bstSearch EmptyLeaf a = False
 bstSearch (Node l a r) b
     | b == a = True
     | b /= a = (bstSearch l b) || (bstSearch r b)
 
--- check if a binary tree is a BST
-largestChild :: (Ord a) => BST a -> Maybe a
-largestChild t =
-    case t of 
-        EmptyLeaf                  -> Nothing
-        Node EmptyLeaf a EmptyLeaf -> Just a
-        Node l a EmptyLeaf         -> Just a
-        Node EmptyLeaf a r         -> largestChild r
-        Node l a r                 -> largestChild r
+-- remove value (delete) using zippers
 
+-- convert in-order to list
 
+-- convert pre-order to list
 
--- remove (delete)
-
--- convert BST to list
+-- convert post-order to list
 
 -- convert list to BST
